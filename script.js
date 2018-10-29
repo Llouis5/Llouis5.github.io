68752145546d721be6120270cca1bd95b9e90df5
Unknown <louis.potron@efrei.net>
2018-10-29 05:11:10 +0100

    Added task 6

--- script.js
+++ script.js
@@ -18,6 +18,21 @@ function init()
 
     for (var i = spans.length - 1; i >= 0; i--) {
         spans[i].style.color = colors[i];
-}
+    }
+    
+    var changeSrc = function (event) {
+        if (event.target.src) {
+            let filename = event.target.src.replace(/^.*[\\\/]/, '');
+            if (filename == "flower1.png") {
+                event.target.src = "Icons/flower2.png";
+            } else {
+                event.target.src = "Icons/flower1.png";
+            }
+        }
+        
+        
+    };
 
+    document.getElementById("event").addEventListener("mouseover", changeSrc);
+    /*let filename = event.target.src.replace(/^.*[\\\/]/, '');*/
 }
